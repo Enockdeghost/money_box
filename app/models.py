d48ef16570588447c7962b60603a55a2aa445c87
enock_stephano <enockdeghost@gmail.com>
2026-03-31 12:15:30 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+#modelus
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
